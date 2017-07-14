9552a5baf7059cf51bdf3d7a5aa3a7b6aa9201ed
Leandro.Hoffer <leandrohoffer@live.com>
2017-07-13 21:08:54 -0300

    Update CrearPrendas

---
+++ Prendas_Client/src/rawTest/preciosTest.java
@@ -0,0 +1,14 @@
+package rawTest;
+
+public class preciosTest {
+
+	public static void main(String[] args) {
+		int tiempo = 1;
+		float precio = 0;
+		precio = 1;
+		tiempo = (int) (precio*1.3);
+		System.out.println(precio);
+		
+	}
+
+}
