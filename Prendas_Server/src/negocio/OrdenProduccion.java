212a1916048d8d8efdff659473d03da10b512b4c
Leandro <leandrohoffer@live.com>
2017-07-15 23:56:25 -0300

    Sigo copiando cosas de hoy

--- Prendas_Server/src/negocio/OrdenProduccion.java
+++ Prendas_Server/src/negocio/OrdenProduccion.java
@@ -111,6 +111,64 @@ public class OrdenProduccion {
 		this.nro = nro;
 	}
 	
-	
+	public void validarInsumos() {
+		// TODO Auto-generated method stub
+		List<ItemReceta> items= new ArrayList<ItemReceta>();
+		
+		/*Convierto las prendas en sus items, asi tengo directamente un vector de TODOS los items (insumos repetidos)*/
+		for (Prenda p: this.prendas)
+		{
+			for (ItemReceta i: p.getItemsReceta())
+			{
+				items.add(i);
+			}
+		}
+		//Por cada item, primero pregunto si ya esta en mis items resultado, si no esta, lo agrego
+		//y si ya esta, le sumo la cantidad que necesito a la cantidad total que necesito. Mis items resultado va a ser
+		//una lista de insumos DIFERENTES y la cantidad que necesito EN TOTAL para completar esa orden.
+		List<ItemPedidoI> itemspedidoaux= new ArrayList<ItemPedidoI>();
+		for (ItemReceta ir: items)
+		{
+			if (ir.getInsumo().noEstaEn(itemspedidoaux))
+			{
+				ItemPedidoI ip= new ItemPedidoI(ir.getInsumo());
+				ip.setCantidad(ir.getCantidad());
+				itemspedidoaux.add(ip);
+			}
+			else
+			{
+				ir.getInsumo().agregarA(itemspedidoaux, ir.getCantidad());
+			}
+		}
+		//Recorro estos items resultado (itemspedidoaux) verificando si tengo stock de cada uno
+		//Agrego a insumosSinStock los items pedido de los cuales no tengo suficiente stock de insumo
+		//para cubrir la cantidad TOTAL de ese insumo que necesito.
+		List<ItemPedidoI> insumosSinStock= new ArrayList<ItemPedidoI>();
+		for (ItemPedidoI ipp: itemspedidoaux)
+		{
+			if (Almacen.getInstancia().traerStockInsumo(ipp.getInsumo())<ipp.getCantidad())
+				{
+					ipp.setCantidad(ipp.getInsumo().getCantAComprar());
+					insumosSinStock.add(ipp);
+				}
+		}
+		//Creo el pedido de insumos con estos insumos sin stock
+		if (!insumosSinStock.isEmpty())
+		{
+			PedidoInsumo p= new PedidoInsumo();
+			p.setFechaGen(new Date());
+			p.setItems(insumosSinStock);
+			p.setNumero(10); //cambiar esto por los autogenerados
+			p.setOpProveniente(this);
+			p.saveMe();
+			
+		}
+		else
+		{
+			this.setEstado("Completa");
+			//Aca hay que ver que corno pasa cuando completas una orden usea
+		}
+		
+	}
 	
 }
