8b88ee4da4553f246bd41f4ca181372ef31eae95
Leandro <leandrohoffer@live.com>
2017-07-15 23:49:55 -0300

    Lo nuevo que hice hoy

--- Prendas_Server/src/daos/UbicacionDAO.java
+++ Prendas_Server/src/daos/UbicacionDAO.java
@@ -43,4 +43,26 @@ public class UbicacionDAO {
 		}
 		return res;
 	}
+	
+	
+	public UbicacionPrenda traerUbicacionMasAlta()
+	{
+		try
+		{
+			SessionFactory sf = HibernateUtil.getSessionFactory();
+			Session s = sf.openSession();
+			List<UbicacionPrendaEntity> ubicaciones;
+			String hql = "from UbicacionPrendaEntity upe order by upe.ubicacion desc";
+			ubicaciones = s.createQuery(hql).list();
+			UbicacionPrendaEntity resultado = ubicaciones.get(0);
+			return resultado.toNegocio();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
 }
